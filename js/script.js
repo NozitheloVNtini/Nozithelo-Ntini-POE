f19132b599317a3fa0a7730d63315941f19aa817
Nozithelo <st10519223@rcconnect.edu.za>
2026-06-17 18:41:23 +0200

    Fixed desktop nav visibility and enabled mobile hamburger menu functionality on small screens

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//sticky navigation tab when scrolling up
+// sticky navigation tab when scrolling up
 document.addEventListener("DOMContentLoaded", () => {
   let lastScrollY = window.scrollY;
   const nav = document.querySelector(".main-nav");
@@ -7,11 +7,22 @@ document.addEventListener("DOMContentLoaded", () => {
     const currentScrollY = window.scrollY;
 
     if (currentScrollY > lastScrollY && currentScrollY > 80) {
+      nav.classList.remove("open"); // close hamburger menu
       nav.classList.add("nav-hidden");
     } else {
       nav.classList.remove("nav-hidden");
     }
 
     lastScrollY = currentScrollY;
+  });
+});
+
+// hamburger menu on mobile screen
+document.addEventListener("DOMContentLoaded", () => {
+  const nav = document.querySelector(".main-nav");
+  const toggle = document.querySelector(".nav-toggle");
+
+  toggle.addEventListener("click", () => {
+    nav.classList.toggle("open");
   });
 });
